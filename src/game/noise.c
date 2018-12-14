5bff11ead295829623b6dde2edef9f86eae5df87
Joshua D Katz <gravypod@gravypod.com>
2018-12-13 20:09:34 -0500

    Use Simplex Noise Generate Terrain.

--- src/game/noise.c
+++ src/game/noise.c
@@ -1,9 +1,21 @@
 #include "noise.h"
+#include "coords.h"
+#include <open-simplex-noise.h>
+#include <stddef.h>
+
+struct osn_context *ctx = NULL;
+
 
 bool noise_block_exists(long seed, long x, long y, long z)
 {
+    if (ctx == NULL) {
+        open_simplex_noise(seed, &ctx);
+    }
+
+    double height = (((SIZE_CHUNK_Y - 40) / 10) * fabs(open_simplex_noise2(ctx, x / ((double) SIZE_CHUNK_X), z / ((double) SIZE_CHUNK_Z)))) + 40;
+
     // Chosen by random dice roll
-    if (y < 40) {
+    if (y <= height) {
         return true;
     }
 
@@ -12,7 +24,9 @@ bool noise_block_exists(long seed, long x, long y, long z)
 
 block_t *noise_block_type(long seed, long x, long y, long z)
 {
-    if (y < 10) {
+    double type = round(open_simplex_noise3(ctx, x, y, z));
+
+    if (type < 0) {
         return block_stone;
     }
 
